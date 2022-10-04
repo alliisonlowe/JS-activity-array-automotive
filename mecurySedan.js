e132424bdcb0a174888cc3d842f621ed3a6e326a
Allison Lowe <alliisonlowe@gmail.com>
2022-10-04 01:07:03 -0700

    adding class and constructor

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,6 +1,26 @@
 //this includes the vehicle class as a module
 const VehicleModule = require("./vehicleBaseClass")
 
+class Car extends VehicleModule {
+    constructor(make, model, year, color, mileage) {
+        super(make, model, year, color, mileage);
+        this.maxPassengers = 5;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maxSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+    }
+
+
+checkService() {
+    if (this.mileage > 3000) {
+        this.scheduleService = true;
+        return this.scheduleService;
+    }
+}
+
+}
 //this shows how to call from this module...
 let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
